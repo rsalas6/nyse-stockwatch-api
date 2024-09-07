d7a0d6480edb949821c73300af91479e8b50ba68
Roberto Salas <hi@beto.page>
2024-09-07 10:27:55 -0600

    fix: exclude token for non docs

--- nyse/middlewares/auth_middleware.py
+++ nyse/middlewares/auth_middleware.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.conf import settings
-
+from django.urls import resolve
 
 class TokenAuthMiddleware:
     def __init__(self, get_response):
@@ -8,10 +8,13 @@ class TokenAuthMiddleware:
         self.api_token = settings.API_ACCESS_TOKEN
 
     def __call__(self, request):
-        token = request.headers.get("Authorization")
+        excluded_paths = ["/swagger/", "/redoc/", "/swagger.json", "/admin/"]
+        current_path = resolve(request.path_info).route
 
-        if not token or token != f"Bearer {self.api_token}":
-            return JsonResponse({"detail": "Unauthorized: Invalid token"}, status=401)
+        if not any(path in current_path for path in excluded_paths):
+            token = request.headers.get("Authorization")
+            if not token or token != f"Bearer {self.api_token}":
+                return JsonResponse({"detail": "Unauthorized: Invalid token"}, status=401)
 
         response = self.get_response(request)
         return response
